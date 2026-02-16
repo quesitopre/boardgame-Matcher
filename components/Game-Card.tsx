630a2d78caba25a5d57c6cde219ce95b3ffb0f16
alfredoleano <alfredoleano6@gmail.com>
2026-02-15 19:57:55 -0800

    Playing around with styling using flex-1. Moving text to the right side of game cover.

--- components/Game-Card.tsx
+++ components/Game-Card.tsx
@@ -3,10 +3,10 @@ export default function GameCard({ title, image, players, description }: { title
     <main>
       <div className="bg-red-100">
         <div className="flex justify-around items-center">
-          <img src={image} className="h-60 bg-amber-100" />
-          <div className="text-left">
-            <h2 className="bg-amber-500 h-fit text-2xl">{title}</h2>
-            <h3 className="bg-amber-500 h-fit text-lg">{players} Players</h3>
+          <img src={image} className="flex-1 h-60 bg-amber-100" />
+          <div className="flex-1 text-left space-y-2">
+            <h2 className="bg-amber-500 h-fit w-fit text-2xl font-bold">{title}</h2>
+            <h3 className="bg-amber-500 h-fit w-fit text-lg">{players} Players</h3>
           </div>
         </div>
         <p>{description}</p>
