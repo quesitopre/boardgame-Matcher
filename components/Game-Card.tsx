16b2886370544b2c8e331deb2e8b9a58116a897b
alfredoleano <alfredoleano6@gmail.com>
2026-02-15 19:48:09 -0800

    Playing around with styling of card. Moving text to the right of game covers.

--- components/Game-Card.tsx
+++ components/Game-Card.tsx
@@ -1,9 +1,14 @@
-export default function GameCard({ title, image, description }: { title: string; image: string; description: string }) {
+export default function GameCard({ title, image, players, description }: { title: string; image: string; players: string; description: string }) {
   return (
-    <main className="bg-green-200">
-      <div>
-        <img src={image} className="h-70" />
-        <h3>{title}</h3>
+    <main>
+      <div className="bg-red-100">
+        <div className="flex justify-around items-center">
+          <img src={image} className="h-60 bg-amber-100" />
+          <div className="text-left">
+            <h2 className="bg-amber-500 h-fit text-2xl">{title}</h2>
+            <h3 className="bg-amber-500 h-fit text-lg">{players} Players</h3>
+          </div>
+        </div>
         <p>{description}</p>
       </div>
     </main>
