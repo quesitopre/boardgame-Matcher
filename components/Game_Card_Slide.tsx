a18bdc1705407095fd616f4e1da04dbadeeed24b
alfredoleano <alfredoleano6@gmail.com>
2026-03-03 21:09:07 -0800

    Revamped the card slide. Changed array to array of objects to generate GameCard components more efficiently. Implementing state and the spread operator.

--- components/Game_Card_Slide.tsx
+++ components/Game_Card_Slide.tsx
@@ -1,15 +1,31 @@
+'use client'
+import { useState } from 'react'
 import GameCard from "./Game-Card"
 
-export default function GameCardSlide() {
-    let game_cards = [
-        <GameCard title="Catan" image="Catan-Cover.png" players="3-4" rating={5} weight="Medium" tag_1="Strategy" tag_2="Trade" game_description="Collect and trade resources to build up the island of Catan in this modern classic." />,
-        <GameCard title="Codenames" image="Codenames-Cover.jpg" players="4-12" rating={4.5} weight="Light" tag_1="Words" tag_2="Teamwork" game_description="Give your team clever one-word clues to help them spot their agents in the field. " />,
-        <GameCard title="King of Tokyo" image="King_of_Tokyo_Cover.webp" players="3-6" rating={4} weight="Light" tag_1="Dice" tag_2="Fighting" game_description="Prove your dominance by destroying Tokyo or by being the last monster left standing." />
+// Not in function because the data doesn't change. If it's inside the function, React would recreate array in memory on every render which is wasteful.
+const games = [
+        {title:"Catan", image:"Catan-Cover.png", players:"3-4", rating:5, weight:"Medium", tag_1:"Strategy", tag_2:"Trade", game_description:"Collect and trade resources to build up the island of Catan in this modern classic."}, 
+        {title: "Codenames", image:"Codenames-Cover.jpg", players: "4-12", rating: 4.5, weight: "Light", tag_1: "Words", tag_2: "Teamwork", game_description: "Give your team clever one-word clues to help them spot their agents in the field."}, 
+        {title: "King of Tokyo", image: "King_Of_Tokyo_Cover.webp", players: "3-6", rating: 4, weight: "Light", tag_1: "Dice", tag_2: "Fighting", game_description: "Prove your dominance by destroying Tokyou or by being the last monster left standing."}
     ]
+export default function GameCardSlide() {
+    const [currentIndex, setCurrrentIndex] = useState(0)
+
+    if (currentIndex >= games.length) {
+        return <div>No more games!</div>
+    }
 
     return (
-        <main>
-            {game_cards}
-        </main>
+        <div>
+            {/* Top card */}
+            <div>
+                <GameCard {...games[currentIndex]} />
+            </div>
+
+            {/* Render next card underneath */}
+            <div>
+                <GameCard {...games[currentIndex + 1]} />
+            </div>
+        </div>
     )
 }
